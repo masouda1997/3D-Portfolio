6d7b1239677370272cda636554ad32fa587aca47
masoud <masoud30376@gmail.com>
2023-04-29 09:48:04 +0430

    hero text section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<section className="relative w-full mx-auto h-screen ">
+			<div
+				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 `}
+			>
+				{/* purple vertical line */}
+				<div className="flex flex-col justify-center items-center mt-5 ">
+					<div className="w-5 h-5 bg-[#915eff] rounded-full" />
+					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+				</div>
+				<div>
+					<h1 className={`${styles.heroHeadText}`}>
+						Hi, I'm <span className="text-[#915eff]">Masoud</span>
+					</h1>
+					<p className={`${styles.heroSubText} text-white-100 mt-2`}>
+						I'm a Front-end web developer{" "}
+						<br className="sm:block" /> designing 3D user interfaces
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default Hero
+export default Hero;

a5a4f5ecf666233c7036aabe4e791b7bf427eb5b
masoud <masoud30376@gmail.com>
2023-05-12 16:14:42 +0430

    the emailjs service added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,12 +6,49 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 import emailjs from "@emailjs/browser";
 
+// btXg9fDyND389Vcis
+// template_e89nwid
+// service_bfle3a3
+
 const Contact = () => {
 	const formRef = useRef();
 	const [form, setForm] = useState({ name: "", email: "", message: "" });
 	const [loading, setLoading] = useState(false);
-	const handleChange = (e) => {};
-	const handleSubmit = (e) => {};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setForm({ ...form, [name]: value });
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+		emailjs
+			.send(
+				"service_5ywedpq",
+				"template_e89nwid",
+				{
+					from_name: form.name,
+					to_name: "Masoud",
+					from_email: form.email,
+					to_Email: "masoud30376@gmail.com",
+					message: form.message,
+				},
+				"here put your public key witch provided by Emailjs"
+			)
+			.then(
+				() => {
+					setLoading(false);
+					alert(
+						"Thank you I will get back to you as soon as possible."
+					);
+					setForm({ name: "", email: "", message: "" });
+				},
+				(error) => {
+					setLoading(false);
+					console.log(error);
+					alert("Something went wrong.");
+				}
+			);
+	};
 	return (
 		<div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
 			<motion.div
@@ -22,8 +59,8 @@ const Contact = () => {
 				<h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
 				<form
 					ref={formRef}
-					onSubmit={handleChange}
-					className="mt-12 flex flex-col gap-8"
+					onSubmit={handleSubmit}
+					className="mt-12 flex flex-1 flex-col gap-8"
 				>
 					<label className="flex flex-col">
 						<span className="text-white font-medium mb-4">
